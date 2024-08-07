7871948a67e958c783cd73f648ee289d74dc1c53
brynnb <brynnb@users.noreply.github.com>
2024-08-07 15:57:49 -0500

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import GameGrid from "./components/GameGrid";
 import GameDetailPage from "./components/GameDetailPage";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "/games", element: <GameGrid /> },
