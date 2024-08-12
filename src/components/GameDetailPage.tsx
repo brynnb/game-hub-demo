bcc4ebbf3baa42252a69e252de898e6b27ac2b29
brynnb <brynnb@users.noreply.github.com>
2024-08-12 12:35:33 -0500

    Add game trailer

--- src/components/GameDetailPage.tsx
+++ src/components/GameDetailPage.tsx
@@ -5,6 +5,7 @@ import CriticScore from "./CriticScore";
 import DefinitionItem from "./DefinitionItem";
 import ExpandableText from "./ExpandableText";
 import GameAttributes from "./GameAttributes";
+import GameTrailer from "./GameTrailer";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -19,6 +20,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
